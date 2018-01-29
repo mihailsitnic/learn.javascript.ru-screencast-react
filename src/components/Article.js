edfa74491b606a7bff81bb5ca7ac0568a342d12a
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-29 14:18:56 +0200

    Interactivity, component state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Article(props) {
-    const { article } = props
-    console.log('---', props)
-    const body = <section>{article.text}</section>
-    return (
-        <div className="hello" style={{color: 'red'}}>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    state = {
+        isOpen: false
+    }
+    render() {
+        const { article } = this.props
+        const body = this.state.isOpen && <section>{article.text}</section>
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
 
 export default Article
